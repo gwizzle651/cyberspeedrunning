722a4e7e4cdc552b22e969b5d7b200cf030e0faa
JimBobJoe36 <188243628+JimBobJoe36@users.noreply.github.com>
2025-12-16 15:05:05 -0600

    Basic linking and titles

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 <html>
     <head>
         <title>Cyber Speedrunning</title>
+        <link rel="stylesheet" href="assets/style/style.css">
     </head>
-
     <body>
-
+        <h1 class="Title">Cyber Speedrun</h1>
     </body>
 </html>
