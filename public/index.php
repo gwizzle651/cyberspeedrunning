fecb6da1c686f2ccfcb0860426589fbd63e1748b
gwizzle651 <gwizzle651@proton.me>
2025-12-16 21:44:47 -0600

    basic functionality for index.php
    testing is the apache web server is spinning up correctly. obviously remove all of this nonsense later.

--- public/index.php
+++ public/index.php
@@ -1,10 +1,31 @@
+<?php
+// PHP code can go here
+$title = "super epic cyber speedrunning site";
+?>
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Cyber Speedrunning</title>
-        <link rel="stylesheet" href="assets/style/style.css">
-    </head>
-    <body>
-        <h1 class="Title">Cyber Speedrun</h1>
-    </body>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="assets/style/style.css">
+    <script>
+        function greetUser() {
+            alert("get out of my swamp");
+            
+        }
+    </script>
+    <style>
+        #myImage {
+            display: none;
+            width: 300px;
+            height: auto;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+    <button onclick="showScaryImage()">dont click me; you will get really scared</button>
+    <img src="/assets/images/scaryImage.jpg">
+</body>
 </html>
