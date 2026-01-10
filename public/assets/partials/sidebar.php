21db49d4f8f4ad42da88c2c17246130a9a6fa9e7
garply <the.cooler.garply@gmail.com>
2026-01-10 12:19:42 -0600

    Set up links on the sidebar

--- public/assets/partials/sidebar.php
+++ public/assets/partials/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Partial for the sidebar header used across the site -->
 <?php
     // Set fallback values for vars if they are not set in the including file
-    $category = $category ?? "Unknown";
+    $category = $category ?? "unknown";
 
     // TODO: get login status
 ?>
@@ -10,16 +10,15 @@
     <header class="header">
         <h1 class="h1-centered">The Cyber Speedrunning Leaderboard</h1>
     </header>
-
+    <!-- TODO: make current location have highlight border -->
     <nav class="nav-menu-v">
-    <!-- TODO: add actual links -->
-        <a class="h4">Dashboard</a>
-        <a class="h4">Leaderboards</a>
-        <a class="h4">Help</a>
+        <a href="https://bingleton.org/dashboard.php" class="h4">Dashboard</a>
+        <a href="https://bingleton.org/leaderboards.php" class="h4">Leaderboards</a>
+        <a href="https://bingleton.org/help.php" class="h4">Help</a>
     </nav>
 
     <footer class="footer">
     <!-- TODO: check login status and display user profile -->
-        <a class="button-l-highlight h4-centered w-100">Login</a>
+        <a href="https://bingleton.org/account/login.php" class="button-l-highlight h4-centered w-100">Login</a>
     </footer>
 </div>
