7bbdc2c62b6f121e9d212cb685530f8819c85a3e
gwizzle651 <gwizzle651@proton.me>
2026-01-16 12:46:59 -0600

    add comments in file

--- public/assets/scripts/form-handler.php
+++ public/assets/scripts/form-handler.php
@@ -1,17 +1,23 @@
 <?php
 if ($_POST["username"]) {
-	$username = $_POST["username"];
+	$username = htmlspecialchars($_POST["username"]);
 }
 
 if ($_POST["password"]) {
-	$plainTextPassword = $_POST["password"];
+	$plainTextPassword = htmlspecialchars($_POST["password"]);
+}
+
+if ($_POST["confirm-password"]) {
+	$plainTextPassword = htmlspecialchars($_POST["confirm-password"]);
 }
 
 if ($_POST["runLink"]) {
-	$runLink = $_POST["runLink"];
+	$runLink = htmlspecialchars($_POST["runLink"]);
 }
 
 if ($_POST["runTime"]) {
-	$runTime = $_POST["runTime"];
+	$runTime = htmlspecialchars($_POST["runTime"]);
 }
 
+// figure out how to identify which form the data is coming from.
+// write functions to interact with the database. 
