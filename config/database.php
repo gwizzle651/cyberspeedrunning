1c95871f9ca2aa245ae429328434f6352522344f
JimBobJoe36 <188243628+JimBobJoe36@users.noreply.github.com>
2026-01-16 11:57:54 -0600

    Restructured database.php for better security

--- config/database.php
+++ config/database.php
@@ -1,12 +1,22 @@
 <?php
 declare(strict_types=1);
 
-$dbHost = 'localhost';
-$dbName = 'cyber_speedrunning'; 
-$dbUser = 'dbAdmin'; 
-$dbPass = ''; 
+// Read the file
+
+// BEST PRACTICE (Run this in BASH)
+# composer require vlucas/phpdotenv
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$dbHost = 'localhost'; # Name of the dbHost in the .env
+$dbName = getenv('DB_NAME'); # Name of the dbName in the .env
+$dbUser = getenv('DB_USER'); # Name of the dbUser in the .env 
+$dbPass = getenv('DB_PASS'); # Name of the dbPass in the .env
 $dbCharset = 'utf8mb4';
 
+// DO NOT TOUCH ANYTHING BELOW
 $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=$dbCharset";
 
 $options = [
